9ab5a3b7530737d79d824716e3d689aefa19d192
teotoivo <teo.maximilien@gmail.com>
2025-05-29 15:03:23 +0300

    kernel: remove old boot entry point and header

--- kernel/arch/x86_64/boot.c
+++
@@ -1,6 +0,0 @@
-#include <limine.h>
-
-__attribute__((used, section(".limine_requests"))) volatile LIMINE_BASE_REVISION(3);
-
-__attribute__((used, section(".limine_requests"))) volatile struct limine_framebuffer_request
-    framebuffer_request = {.id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 0};
--- kernel/include/arch/x86_64/boot.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <limine.h>
-
-// Accessible from anywhere that needs to read framebuffer, etc.
-extern volatile struct limine_framebuffer_request framebuffer_request;
-extern volatile uint64_t limine_base_revision[];
